e9c4a099bcdd24ae99dab54c9d134d8597f038d3
Diego Hennrich <diegohennrich@gmail.com>
2020-04-16 14:38:13 -0300

    Last Commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,75 @@ app.use(cors());
 
 const repositories = [];
 
+function idIsValid(request, response, next) {
+  const { params } = request;
+  const { id } = params;
+
+  if (!isUuid(id)) return response.status(400).json({ error: "Invalid ID." });
+
+  if (!repositories.find(i => i.id === id))
+    return response.status(400).json({ error: "Repository not found." });
+
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { body } = request;
+  const { title, url, techs } = body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", idIsValid, (request, response) => {
+  const { body, params } = request;
+  const { id } = params;
+  const { title, url, techs } = body;
+
+  const index = repositories.findIndex(i => i.id === id);
+
+  repositories[index].title = title;
+  repositories[index].url = url;
+  repositories[index].techs = techs;
+
+  // estou fazendo dessa forma, pois se substituir todo o objeto pelo novo ele irá alterar os likes.
+
+  response.json(repositories[index]);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", idIsValid, (request, response) => {
+  const { params } = request;
+  const { id } = params;
+
+  const index = repositories.findIndex(i => i.id === id);
+
+  repositories.splice(index, 1);
+
+  response.status(204).json();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", idIsValid, (request, response) => {
+  const { params } = request;
+  const { id } = params;
+
+  const index = repositories.findIndex(i => i.id === id);
+
+  repositories[index].likes = repositories[index].likes + 1;
+
+  response.json(repositories[index]);
 });
 
 module.exports = app;
